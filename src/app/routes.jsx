0469b1b92e3e15d83f6f46700c490973d95a1388
Petar Volic <eaa25pevo@students.eaaa.dk>
2026-05-20 00:19:17 +0200

    Bug fixes

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,7 +1,7 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import Layout from "./components/Layout";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     Component: Layout,
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Final-exam-BLA-SOL-Pant/',
   plugins: [
     react(),
     tailwindcss(),
